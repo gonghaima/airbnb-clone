733965008f976fef2b0ea473445c27b6ad1bd072
Steven Gong <gonghaima@hotmail.com>
2022-12-31 15:48:01 +1100

    prepare header component

---
+++ components/Header.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from 'react';
+
+const Header: FunctionComponent = () => {
+  return (
+    <div>
+      <h1>I am the header</h1>
+
+      {/* left */}
+      <div></div>
+
+      {/* middle */}
+      <div></div>
+
+      {/* right */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
