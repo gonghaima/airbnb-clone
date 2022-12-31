38867a9b426992b90f56594b14b9315f6171b036
Steven Gong <gonghaima@hotmail.com>
2022-12-31 16:55:38 +1100

    header left

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,25 @@
 import React, { FunctionComponent } from 'react';
+import Image from 'next/legacy/image';
 
 const Header: FunctionComponent = () => {
   return (
-    <div>
-      <h1>I am the header</h1>
-
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
       {/* left */}
-      <div></div>
+      <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
 
       {/* middle */}
       <div></div>
 
       {/* right */}
       <div></div>
-    </div>
+    </header>
   );
 };
 
