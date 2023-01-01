a4ceb87463f755958a8f36d79b25d7b92e990c5a
Steven Gong <gonghaima@hotmail.com>
2023-01-01 16:54:01 +1100

    header right

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,12 @@
 import React, { FunctionComponent } from 'react';
 import Image from 'next/legacy/image';
-import { SearchIcon } from '@heroicons/react/solid';
+import {
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+  SearchIcon,
+} from '@heroicons/react/solid';
 
 const Header: FunctionComponent = () => {
   return (
@@ -16,15 +22,28 @@ const Header: FunctionComponent = () => {
       </div>
 
       {/* middle */}
-      <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
-        <input className='pl-5 bg-transparent outline-none flex-grow
-        text-sm text-gray-600 placeholder-gray-400' type="text" placeholder="Start your seasrch" />
-        <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white
-         rounded-full p-2 cursor-pointer md:mx-2'/>
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          className="pl-5 bg-transparent outline-none flex-grow
+        text-sm text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder="Start your seasrch"
+        />
+        <SearchIcon
+          className="hidden md:inline-flex h-8 bg-red-400 text-white
+         rounded-full p-2 cursor-pointer md:mx-2"
+        />
       </div>
 
       {/* right */}
-      <div></div>
+      <div className="flex items-center space-x-4 justify-end text-gray-500">
+        <p className='hidden md:inline cursor-pointer'>Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 };
