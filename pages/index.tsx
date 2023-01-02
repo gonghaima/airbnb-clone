ca3a5be679738558226cbe05b32f67999d68f517
Steven Gong <gonghaima@hotmail.com>
2023-01-02 14:24:10 +1100

    Explore section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { GetStaticProps } from 'next';
 import { Inter } from '@next/font/google';
 import styles from '../styles/Home.module.css';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
+import { IExplore } from '../types';
+import SmallCard from '../components/SmallCard';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+export default function Home({
+  exploreData,
+}: {
+  exploreData: IExplore[];
+}): JSX.Element {
   return (
     <>
       <Head>
@@ -16,9 +23,85 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
-      <Banner/>
+      <Header />
+      <Banner />
       {/* main section*/}
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Expore Nearby</h2>
+          {/* Pull some data from a server - A
+          PI endpoint */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ img, distance, location }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
+        </section>
+      </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps<{
+  exploreData: IExplore[];
+}> = async () => {
+  // must be async
+  // const exploreDataMock = await fetch('https://links.papareact.com/pyp').then(
+  //   (res) => res.json()
+  // );
+  const exploreData = await fetch('https://www.jsonkeeper.com/b/32SI').then(
+    (res) => res.json()
+  );
+  // const exploreData = [
+  //   {
+  //     img: 'https://links.papareact.com/5j2',
+  //     location: 'London',
+  //     distance: '45-minute drive',
+  //   },
+  //   {
+  //     img: 'https://links.papareact.com/1to',
+  //     location: 'Manchester',
+  //     distance: '4.5-hour drive',
+  //   },
+  //   {
+  //     img: 'https://links.papareact.com/40m',
+  //     location: 'Liverpool',
+  //     distance: '4.5-hour drive',
+  //   },
+  //   {
+  //     img: 'https://links.papareact.com/msp',
+  //     location: 'York',
+  //     distance: '4-hour drive',
+  //   },
+  //   {
+  //     img: 'https://links.papareact.com/2k3',
+  //     location: 'Cardiff',
+  //     distance: '45-minute drive',
+  //   },
+  //   {
+  //     img: 'https://links.papareact.com/ynx',
+  //     location: 'Birkenhead',
+  //     distance: '4.5-hour drive',
+  //   },
+  //   {
+  //     img: 'https://links.papareact.com/kji',
+  //     location: 'Newquay',
+  //     distance: '6-hour drive',
+  //   },
+  //   {
+  //     img: 'https://links.papareact.com/41m',
+  //     location: 'Hove',
+  //     distance: '2-hour drive',
+  //   },
+  // ];
+  return {
+    props: {
+      exploreData,
+    },
+  };
+};
---
+++ types/index.ts
@@ -0,0 +1,5 @@
+export interface IExplore {
+  img: string;
+  location: string;
+  distance: string;
+}
